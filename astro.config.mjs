c6c8b39ad3611f6a608cd2b7938cb309bb91e8de
DESKTOP-AFQD9B7\rodri <rodripb4@hotmail.com>
2024-04-29 11:37:18 +0100

    FEAT: Commit final con la adicion de un error 404 y con un generador de paginas dinamico

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: 'server' // En el caso de que queramos generar rutas de forma dinamicas
 });
